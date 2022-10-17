78085d7d3e86f099fed3b5963aad933962552114
realtobyfu <102398824+realtobyfu@users.noreply.github.com>
2022-10-17 04:45:45 -0400

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,15 +1,33 @@
-var h, f, s;
-h = prompt("How many hotdogs do you want?");
-f = prompt("How many fries do you want?"); 
-s = prompt("How many sodas do you want?");
-var tax, total, discount = 0, subtotal = 4.25 * h + 2.6 * f + 1.5 * s;
-if (subtotal > 20) {
-  discount = subtotal * .1;
-  subtotal = subtotal - discount;
+function read(){
+  var hot = document.getElementById("hot");
+  var h = hot.options[hot.selectedIndex].value;
+  var fr = document.getElementById("fr");
+  var f = fr.options[fr.selectedIndex].value;
+  var sd = document.getElementById("sd");
+  var s = sd.options[sd.selectedIndex].value;
+  calculate(h, f, s);
 }
-tax = subtotal * .0625;
-total = subtotal + tax;
-discount = discount.toFixed(2);
-subtotal = subtotal.toFixed(2);
-tax = tax.toFixed(2);
-total = total.toFixed(2);
+
+
+function calculate (h, f, s) {
+  var tax, total, discount = 0, subtotal = 4.25 * h + 2.6 * f + 1.5 * s;
+  if (subtotal > 20) {
+    discount = subtotal * .1;
+  }
+  total = subtotal - discount;
+  tax = subtotal * .0625;
+  discount = discount.toFixed(2);
+  subtotal = subtotal.toFixed(2);
+  tax = tax.toFixed(2);
+  total = total.toFixed(2);
+
+  document.getElementById("hotdog").innerHTML = h + " Hotdogs";
+  document.getElementById("fries").innerHTML = f + " French Fries";
+  document.getElementById("soda").innerHTML = s + " Drinks";
+  document.getElementById("subtotals").innerHTML = "Subtotal: $" + subtotal;
+  document.getElementById("tax").innerHTML = "Tax: $" + tax;
+  document.getElementById("total").innerHTML = "Final Total: $" + total;
+  if (subtotal > 20) {
+    document.getElementById("discount (10%)").innerHTML = "Discount: $" + discount;
+  }
+}
